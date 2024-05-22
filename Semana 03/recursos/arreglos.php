2cb4e3a60822330642608b9e28ad158a55ab9e78
danielcruca <daniel.uca.florencio@gmail.com>
2024-05-22 08:37:25 -0600

    agrega archivos de  ejemplo.

---
+++ Semana 03/recursos/arreglos.php
@@ -0,0 +1,24 @@
+<?php
+// Arreglo indexado
+$frutas = array('manzana', 'naranja', 'uva');
+
+// Acceso a elementos por índice
+echo $frutas[0];  // manzana
+echo $frutas[1];  // plátano
+echo $frutas[2];  // uva
+?>
+
+
+<?php
+// Arreglo asociativo
+$persona = array(
+    'nombre' => 'Juan',
+    'edad' => 25,
+    'ciudad' => 'Madrid'
+);
+
+// Acceso a elementos por clave
+echo $persona['nombre'];  // Juan
+echo $persona['edad'];    // 25
+echo $persona['ciudad'];  // Madrid
+?>
---
+++ Semana 03/recursos/operadorTernario.php
@@ -0,0 +1,31 @@
+<?php
+
+# Ejemplo #1 
+// par e impar usando operador ternario.
+$numero = 7;
+
+// Operador ternario para determinar si es par o impar
+$resultado = ($numero % 2 == 0) ? "Par" : "Impar";
+
+// Mostrar el resultado
+echo "El número $numero es: $resultado";
+
+
+
+# Ejemplo #2
+// par e impar usando if normales
+$numero = 7;
+
+// Usando un if para determinar si es par o impar
+if ($numero % 2 == 0) {
+    $resultado = "Par";
+} else {
+    $resultado = "Impar";
+}
+
+// Mostrar el resultado
+echo "El número $numero es: $resultado";
+
+
+
+?>
