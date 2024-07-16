c34794585897ddc43cff336fee760b05175ab174
danielcruca <daniel.uca.florencio@gmail.com>
2024-07-15 20:48:08 -0600

    Agrega cambios

--- Semana 09/recursos/mvc-sencillo-con-db/models/departamentos.php
+++ Semana 09/recursos/mvc-sencillo-con-db/models/departamentos.php
@@ -35,10 +35,10 @@ class departamentos{
             $this->conn = $this->bd->crear_conexion();
             $this->sql = "INSERT INTO DEPARTMENTS (DEPARTMENT_ID, DEPARTMENT_NAME, MANAGER_ID, LOCATION_ID) VALUES (:department_id, :department_name, :manager_id, :location_id)";
             $this->stmt = $this->conn->prepare($this->sql);
-            $this->stmt->bindParam(":department_id", $this->department_id, PDO::PARAM_INT, 4);
-            $this->stmt->bindParam(":department_name", $this->department_name, PDO::PARAM_STR, 30);
-            $this->stmt->bindParam(":manager_id", $this->manager_id, PDO::PARAM_INT, 6);
-            $this->stmt->bindParam(":location_id", $this->location_id, PDO::PARAM_INT, 4);
+            $this->stmt->bindParam(":department_id", $this->department_id);
+            $this->stmt->bindParam(":department_name", $this->department_name);
+            $this->stmt->bindParam(":manager_id", $this->manager_id);
+            $this->stmt->bindParam(":location_id", $this->location_id);
             $this->stmt->execute();
             $this->bd->cerrar_conexion();
             return true;
@@ -53,7 +53,7 @@ class departamentos{
             $this->conn = $this->bd->crear_conexion();
             $this->sql = "DELETE FROM  DEPARTMENTS WHERE DEPARTMENT_ID = :department_id";
             $this->stmt = $this->conn->prepare($this->sql);
-            $this->stmt->bindParam(":department_id", $this->department_id, PDO::PARAM_INT, 4);
+            $this->stmt->bindParam(":department_id", $this->department_id);
             $this->stmt->execute();
             $this->bd->cerrar_conexion();
             return true;
@@ -68,7 +68,7 @@ class departamentos{
             $this->conn = $this->bd->crear_conexion();
             $this->sql = "SELECT * FROM DEPARTMENTS WHERE DEPARTMENT_ID = :department_id";
             $this->stmt = $this->conn->prepare($this->sql);
-            $this->stmt->bindParam(":department_id", $this->department_id, PDO::PARAM_INT, 4);
+            $this->stmt->bindParam(":department_id", $this->department_id);
             $this->stmt->execute();
             $resultado = $this->stmt->fetchAll(PDO::FETCH_ASSOC);
             $this->bd->cerrar_conexion();
@@ -84,10 +84,10 @@ class departamentos{
             $this->conn = $this->bd->crear_conexion();
             $this->sql = "UPDATE DEPARTMENTS SET DEPARTMENT_NAME = :department_name, MANAGER_ID = :manager_id, LOCATION_ID = :location_id WHERE DEPARTMENT_ID = :department_id";
             $this->stmt = $this->conn->prepare($this->sql);
-            $this->stmt->bindParam(":department_id", $this->department_id, PDO::PARAM_INT, 4);
-            $this->stmt->bindParam(":department_name", $this->department_name, PDO::PARAM_STR, 30);
-            $this->stmt->bindParam(":manager_id", $this->manager_id, PDO::PARAM_INT, 6);
-            $this->stmt->bindParam(":location_id", $this->location_id, PDO::PARAM_INT, 4);
+            $this->stmt->bindParam(":department_id", $this->department_id);
+            $this->stmt->bindParam(":department_name", $this->department_name);
+            $this->stmt->bindParam(":manager_id", $this->manager_id);
+            $this->stmt->bindParam(":location_id", $this->location_id);
             $this->stmt->execute();
             $this->bd->cerrar_conexion();
             return true;
